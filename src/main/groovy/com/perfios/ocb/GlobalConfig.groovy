577e9efbe93df8ae99fa468f095748bc619a4eab
sachinbidkikar@gmail.com <sachinbidkikar@gmail.com>
2023-05-18 16:22:02 +0530

    grails project - with Table View

---
+++ src/main/groovy/com/perfios/ocb/GlobalConfig.groovy
@@ -0,0 +1,15 @@
+package com.perfios.ocb
+
+class GlobalConfig {
+
+    public static final def USER_TYPE = [
+            ADMINISTRATOR: "ADMINISTRATOR",
+            REGULAR_MEMBER: "REGULAR_MEMBER",
+    ]
+
+
+    public static Integer itemsPerPage() {
+        return 5
+    }
+
+}
